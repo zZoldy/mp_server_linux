174db22cd6456b64e50280a245b471bd34889005
zZoldy <filipekk41@gmail.com>
2026-01-14 07:02:56 -0300

    Apressentacao 14012025

--- src/main/java/com/app/mirrorpage/server/config/WebSocketConfig.java
+++ src/main/java/com/app/mirrorpage/server/config/WebSocketConfig.java
@@ -31,7 +31,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*");
     }
 
     // 🔴 OBRIGATÓRIO: Registra o interceptador aqui
